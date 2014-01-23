d6e1914ec64654eb0d084b2df2bd596b05626bbc
Yiming Yan Office <yiming.yan@ed.ac.uk>
2014-01-23 14:54:00 +0000

    added scripts that checks if ramdomly generate problems are very likely (~100%) to be primal nondegenerate.

---
+++ Tests/checkDegeneracy.m
@@ -0,0 +1,42 @@
+% This script is used to check if the test problems generate by the
+% function generateRandomProb is primal nondegenerate.
+clc;
+clear;
+
+numTestProb = 100;
+i = 1;
+primal_degen = zeros(numTestProb,1);
+
+while i <= numTestProb
+    
+    [A, b, c] = generateRandomProb('m_min',10,'m_max',200,...
+        'n_min',20,'n_max',500);
+    
+    [m, n]  = size(A);  A  = full(A);
+    lb = zeros(n, 1);   ub = inf*ones(n, 1);
+    
+    % options = optimset('LargeScale', 'off', 'Algorithm','simplex','Display','off');
+    options = optimset('Algorithm','active-set','Display','off');
+    
+    [x,Fval,Exitflag] = linprog(c,[],[],A,b,lb,ub,[],options);
+    
+    if Exitflag ~= 1
+        continue;
+    end
+    
+    if sum(x > 0) < m
+        primal_degen(i) = 1;
+    end
+    
+    clc;
+    progress_bar_length = 100;
+    current_bar_length = round(progress_bar_length*i/numTestProb);
+    output = [repmat('=',1,current_bar_length) '>' repmat(' ', 1, progress_bar_length-current_bar_length)];
+    fprintf('[%s]  - %s %% done.\n',output,num2str(100*i/numTestProb));
+    
+    i = i+1;
+    
+end
+
+disp('# of primal degenerate: ');
+disp(num2str(sum(primal_degen)));
