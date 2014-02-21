be586f5e0864a99d3902167597294f7ace87f36c
Yiming Yan Macbook Pro <yanyiming1986@gmail.com>
2014-02-21 18:18:17 +0000

    nothing changed. just deleted one comment line

--- src/pipm.m
+++ src/pipm.m
@@ -3,14 +3,14 @@ classdef pipm < handle
     % Syntax
     %          p = pipm( A, b, c, parameters_input);
     %          p.solve;
-    % Input: 
-    %          (A, b ,c) - problem data     
-    %          parameters_input: optional. the struct contaitns user defined 
-    %                            paramters. This will overwirte some of the 
-    %                            predifined parameters, including: 
-    %                            maxIter, mu_cap, iPer, cutoff and verbose,
+    % Input:
+    %          (A, b ,c) - problem data
+    %          parameters_input: optional. the struct contaitns user defined
+    %                            paramters. This will overwirte some of the
+    %                            predifined parameters, including:
+    %                            maxIter, tol, mu_cap, iPer, cutoff, verbose,
     %                            actvPredStrtgy.
-    % 
+    %
     % September 16 2013
     % Yiming Yan
     % University of Einburgh
@@ -58,7 +58,7 @@ classdef pipm < handle
         end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %             Main Solver            % 
+        %             Main Solver            %
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         function solve(p)
             % solve - Driver function for the main solver
@@ -70,7 +70,8 @@ classdef pipm < handle
             %% Main loop
             p.output.printHeader(p.prob, p.parameters)
             while ~p.iter.checkTermination(p.counter, p.parameters, p.status);
-                %iter_info(p.counter.iterN+1,:) = [p.counter.iterN p.getMu p.prob.x' p.perturbations.lambda']; save('iter_info.mat','iter_info');
+                % iter_info(p.counter.iterN+1,:) = [p.counter.iterN p.getMu p.prob.x' p.perturbations.lambda']; save('iter_info.mat','iter_info');
+                % iter_x(:,p.counter.iterN+1) = p.prob.x; iter_s(:,p.counter.iterN+1) = p.prob.s; iter_y(:,p.counter.iterN+1) = p.prob.y; save('iter_xys.mat', 'iter_x', 'iter_y', 'iter_s');
                 
                 % Output
                 p.output.printIterations(p.prob, p.counter, p.iter, p.parameters, p.perturbations)
@@ -96,58 +97,63 @@ classdef pipm < handle
                 % Increase counter
                 p.counter.incrementIterationCount;
             end % End while
-            %iter_info(p.counter.iterN+1,:) = [p.counter.iterN p.getMu p.prob.x' p.perturbations.lambda']; save('iter_info.mat','iter_info');
+            % iter_info(p.counter.iterN+1,:) = [p.counter.iterN p.getMu p.prob.x' p.perturbations.lambda']; save('iter_info.mat','iter_info');
+            % iter_x(:,p.counter.iterN+1) = p.prob.x; iter_s(:,p.counter.iterN+1) = p.prob.s; iter_y(:,p.counter.iterN+1) = p.prob.y; save('iter_xys.mat', 'iter_x', 'iter_y', 'iter_s');
             
-            % Output the info of the final ipm iteration 
+            % Output the info of the final ipm iteration
             p.output.printIterations(p.prob, p.counter, p.iter,...
                 p.parameters, p.perturbations);
             p.output.printFooter(p.prob, p.parameters, p.status);
-                        
+            
             % Crossover to simplex if doCrossOver is set to 1
             if p.parameters.doCrossOver
                 p.crossover.splx_solve(p.prob, p.status);
                 p.output.printCrossOver(p.crossover, p.prob, p.parameters, p.status);
             end
-                        
+            
         end
         
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        %            Ultilities              % 
+        %            Ultilities              %
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         function setProbName(p, Name)
-	% setProbName - set the name for the problem
-	    p.prob.setProbName(Name);
-	end
-
+            % setProbName - set the name for the problem
+            p.prob.setProbName(Name);
+        end
+        
         function N = getIPMIterCount(p)
-        % getIPMIterCount - Gets # of ipm iterations
+            % getIPMIterCount - Gets # of ipm iterations
             
             N = p.counter.iterN;
         end
         
         function actv = getActv(p)
-        % getActv - Gets actv from ipm
-        
+            % getActv - Gets actv from ipm
+            
             actv = p.prob.actv;
         end
         
         function final_mu = getMu(p)
-        % final_mu - Gets mu from ipm
-        
+            % final_mu - Gets mu from ipm
+            
             final_mu = p.iter.mu;
         end
         
         
         function residual = getIPMResidual(p)
-        % getIPMResidual - Gets residual from ipm
-        
+            % getIPMResidual - Gets residual from ipm
+            
             residual = p.iter.residual;
         end
         
         function splxIter = getSplxIter(p)
-        % getSplxIter - Gets # of simplex iterations
-           
-	    splxIter = p.crossover.splxIter;  
+            % getSplxIter - Gets # of simplex iterations
+            
+            splxIter = p.crossover.splxIter;
+        end
+        
+        function x = getx(p)
+            x = p.prob.x;
         end
     end
     
