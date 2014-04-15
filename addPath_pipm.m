0f6ac78ffd0acd769c235d33162e266c97500aae
Yiming Yan Macbook Pro <yanyiming1986@gmail.com>
2014-04-15 18:30:56 +0100

    clean files

--- addPath_pipm.m
+++
@@ -1,8 +0,0 @@
-fprintf('Add necessary folders to path... \n');
-
-currentLocation = pwd;
-addpath( currentLocation );
-addpath( genpath( [currentLocation '/src'  ] ) );
-addpath( genpath( [currentLocation '/Examples'  ] ) );
-addpath( genpath( [currentLocation '/thirdParty'] ) );
-addpath( genpath( [currentLocation '/Tests'] ) );
